9466d27e24e8a9dac5dfd6ac36fb577505530ec5
Malaney Hill <malaney@gmail.com>
2017-08-09 15:05:22 -0400

    Setting index of slide to be zero when current index > length of nextProps.children

--- src/inner-slider.js
+++ src/inner-slider.js
@@ -93,7 +93,7 @@ export var InnerSlider = createReactClass({
       this.update(nextProps);
       this.changeSlide({
           message: 'index',
-          index: nextProps.children.length - nextProps.slidesToShow,
+          index: 0,
           currentSlide: this.state.currentSlide
       });
     } else {
